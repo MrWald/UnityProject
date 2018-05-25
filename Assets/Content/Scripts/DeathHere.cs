6f435a0b6c31e4e594f069ccf55871c8724a122e
MrWald <w260698@gmail.com>
2018-05-25 16:18:19 +0300

    Second task
    Death Zone added, movement animation added, parallax effect added

---
+++ Assets/Content/Scripts/DeathHere.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHere : MonoBehaviour {
+
+	//Стандартна функція, яка викличеться, //коли поточний об’єкт зіштовхнеться із іншим
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+//Намагаємося отримати компонент кролика
+		PlayerController rabit = collider.GetComponent<PlayerController>();
+//Впасти міг не тільки кролик
+		if (rabit != null)
+		{
+//Повідомляємо рівень, про смерть кролика
+			LevelController.current.onRabitDeath (rabit);
+		}
+	}
+}
--- Assets/Content/Scripts/PlayerController.cs
+++ Assets/Content/Scripts/PlayerController.cs
@@ -6,12 +6,18 @@ public class PlayerController : MonoBehaviour
 {
 
 	public float speed = 1;
+	public float MaxJumpTime = 2f;
+	public float JumpSpeed = 2f;
+	private bool isGrounded = false;
+	private bool JumpActive = false;
+	private float JumpTime = 0f;
 	
 	Rigidbody2D myBody = null;
 	
 	// Use this for initialization
 	void Start () {
 		myBody = GetComponent<Rigidbody2D>();
+		LevelController.current.setStartPosition (transform.position);
 	}
 	
 	// Update is called once per frame
@@ -21,9 +27,44 @@ public class PlayerController : MonoBehaviour
 	
 	void FixedUpdate () 
 	{ 
+		Animator animator = GetComponent<Animator> (); 
+		Vector3 from = transform.position + Vector3.up * 0.3f;
+		Vector3 to = transform.position + Vector3.down * 0.1f; 
+		int layer_id = 1 << LayerMask.NameToLayer ("Ground");
+//Перевіряємо чи проходить лінія через Collider з шаром Ground
+		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+		if(hit) {
+			isGrounded = true;
+		} else {
+			isGrounded = false;
+		}
+//Намалювати лінію (для розробника)
+		Debug.DrawLine (from, to, Color.red);
+		
+		if(Input.GetButtonDown("Jump") && isGrounded) {
+			this.JumpActive = true; }
+		if(this.JumpActive) {
+//Якщо кнопку ще тримають if(Input.GetButton("Jump")) {
+			this.JumpTime += Time.deltaTime;
+			if (this.JumpTime < this.MaxJumpTime) {
+				Vector2 vel = myBody.velocity;
+				vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime); myBody.velocity = vel;
+			}
+		} else {
+			this.JumpActive = false;
+			this.JumpTime = 0; 
+		}
 		//[-1, 1]
 		float value = Input.GetAxis ("Horizontal");
-		if (Mathf.Abs (value) > 0) {
+		if(this.isGrounded) {
+			animator.SetBool ("jump", false);
+		} else {
+			animator.SetBool ("jump", true);
+		}
+
+		if (Mathf.Abs (value) > 0) 
+		{
+			animator.SetBool ("run", true);
 			Vector2 vel = myBody.velocity;
 			vel.x = value * speed;
 			myBody.velocity = vel;
@@ -37,5 +78,9 @@ public class PlayerController : MonoBehaviour
 				sr.flipX = false;
 			}
 		} 
+		else 
+		{
+			animator.SetBool ("run", false); 
+		}
 	}
 }
